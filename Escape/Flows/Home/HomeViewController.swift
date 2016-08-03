47e2cb57be687205c37000396f14842c0e5f81eb
ankitgarg135 <ankit.garg1@helpchat.in>
2016-08-03 15:55:43 +0530

    correcting heirachy

---
+++ Escape/Flows/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Escape
+//
+//  Created by Ankit on 03/08/16.
+//  Copyright © 2016 EscapeApp. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
