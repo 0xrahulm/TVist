16ab5c5787a55ee360fe14f2743a25450729746c
ankitgarg135 <ankit.garg1@helpchat.in>
2016-10-23 14:42:16 +0530

    article work start

---
+++ Escape/Flows/Home/Cell Views/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  Escape
+//
+//  Created by Ankit on 02/10/16.
+//  Copyright © 2016 EscapeApp. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: BaseStoryTableViewCell {
+
+    
+    
+
+}
